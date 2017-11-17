6b75ebbdb6a108c3c5d2efecd2abca4114bb891d
nooperation <madbrahmin@gmail.com>
2017-11-16 20:18:23 -0500

    Started work on standalone resource viewer

---
+++ SanBag.Viewer/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using SanBag.Viewer.Views;
+
+namespace SanBag.Viewer.ViewModels
+{
+    class MainViewModel
+    {
+        public UserControl CurrentView { get; set; }
+        public BaseViewModel CurrentViewModel { get; set; }
+
+        public MainViewModel()
+        {
+        }
+
+        public void OpenFile(string resourcePath)
+        {
+            var fileName = Path.GetFileName(resourcePath);
+            var fileInfo = LibSanBag.FileRecordInfo.Create(fileName);
+
+            switch (fileInfo.Resource)
+            {
+                case LibSanBag.FileRecordInfo.ResourceType.TextureResource:
+                    CurrentViewModel = new TextureResourceViewModel()
+                    {
+                        CurrentPath = resourcePath
+                    };
+                    CurrentView = new TextureResourceView()
+                    {
+                        DataContext = CurrentViewModel
+                    };
+                    break;
+            }
+
+        }
+    }
+}
