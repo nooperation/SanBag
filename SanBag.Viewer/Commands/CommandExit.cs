325e370b2737977b503e521d7c59bfbad9234cfc
nooperation <madbrahmin@gmail.com>
2017-11-21 20:01:37 -0500

    Added save-as menu for the raw resource viewer

---
+++ SanBag.Viewer/Commands/CommandExit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SanBag.Viewer.Commands
+{
+    class CommandExit : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ SanBag.Viewer/Commands/CommandSaveAs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SanBag.Viewer.ViewModels;
+
+namespace SanBag.Viewer.Commands
+{
+    class CommandSaveAs : ICommand
+    {
+        private readonly ISavable savable;
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public CommandSaveAs(ISavable savable)
+        {
+            this.savable = savable;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            savable.SaveAs();
+        }
+    }
+}
