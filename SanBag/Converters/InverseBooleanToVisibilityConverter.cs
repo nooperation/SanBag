132c5d9ff6cdf14f0df5db6b97432a3b671c134e
nooperation <madbrahmin@gmail.com>
2017-11-24 03:42:46 -0500

    Moved Converters out of Views namespace

--- SanBag/Converters/InverseBooleanToVisibilityConverter.cs
+++ SanBag/Converters/InverseBooleanToVisibilityConverter.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 
-namespace SanBag.Views.Converters
+namespace SanBag.Converters
 {
     class InverseBooleanToVisibilityConverter : IValueConverter
     {
--- SanBag/Converters/SizeToStringConverter.cs
+++ SanBag/Converters/SizeToStringConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace SanBag.Views.Converters
+namespace SanBag.Converters
 {
     public class SizeToStringConverter : IValueConverter
     {
