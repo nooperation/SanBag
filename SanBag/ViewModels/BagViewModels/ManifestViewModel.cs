3fe6462833665f1081a3a0456a9f0004f292a6dc
nooperation <madbrahmin@gmail.com>
2017-12-09 02:46:24 -0500

    Working on extracting manifestresource viewer

--- SanBag/ViewModels/BagViewModels/ManifestViewModel.cs
+++ SanBag/ViewModels/BagViewModels/ManifestViewModel.cs
@@ -22,72 +22,13 @@ namespace SanBag.ViewModels.BagViewModels
 {
     public class ManifestViewModel : GenericBagViewModel, INotifyPropertyChanged
     {
-        public readonly string FilterNone = "None";
-
-        public CommandManifestExportSelected CommandManifestExportSelected { get; set; }
-
-        private List<ManifestEntry> _manifestList = new List<ManifestEntry>();
-
-        public List<ManifestEntry> ManifestList
-        {
-            get => _manifestList;
-            set
-            {
-                _manifestList = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(FilteredManifestList));
-                OnPropertyChanged(nameof(Filters));
-            }
-        }
-
-        public List<ManifestEntry> FilteredManifestList
-        {
-            get
-            {
-                if (CurrentFilter == "None")
-                {
-                    return _manifestList;
-                }
-                return _manifestList.Where(n => n.Name.Contains(CurrentFilter)).ToList();
-            }
-        }
-
-        private string _currentFilter;
-        public string CurrentFilter
-        {
-            get => _currentFilter;
-            set
-            {
-                _currentFilter = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(FilteredManifestList));
-            }
-        }
-
-        public List<string> Filters
-        {
-            get
-            {
-                var foundTypes = new HashSet<string>();
-                foreach (var manifest in ManifestList)
-                {
-                    foundTypes.Add(manifest.Name);
-                }
-                var filters = foundTypes.OrderBy(n => n).ToList();
-                filters.Insert(0, FilterNone);
-
-                return filters;
-            }
-        }
-
+ 
         public ManifestViewModel(BagViewModel parentViewModel)
                 : base(parentViewModel)
         {
             ExportFilter += "|Manifest Dump|*.txt";
-
-            Filters.Add(FilterNone);
-            CurrentFilter = FilterNone;
-            CommandManifestExportSelected = new CommandManifestExportSelected(this);
+            CurrentResourceView = new SanBag.Views.ResourceViews.ManifestResourceView();
+            CurrentResourceView.DataContext = new SanBag.ViewModels.ResourceViewModels.ManifestResourceViewModel();
         }
 
         public override bool IsValidRecord(FileRecord record)
@@ -97,97 +38,94 @@ namespace SanBag.ViewModels.BagViewModels
 
         protected override void OnSelectedRecordChanged()
         {
-            try
-            {
-                using (var bagStream = File.OpenRead(ParentViewModel.BagPath))
-                {
-                    var manifest = new ManifestResource();
-                    manifest.InitFromRecord(bagStream, SelectedRecord);
-                    ManifestList = manifest.Entries;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                ManifestList = new List<ManifestEntry>();
-            }
-        }
-
-        protected override void CustomFileExport(ExportParameters exportParameters)
-        {
-            ManifestResource manifest;
-            using (var bagStream = File.OpenRead(ParentViewModel.BagPath))
-            {
-                manifest = new ManifestResource();
-                manifest.InitFromRecord(bagStream, SelectedRecord);
-                ManifestList = manifest.Entries;
-            }
-
-            var sb = new StringBuilder();
-            sb.AppendLine("Entries:");
-            foreach (var item in manifest.Entries)
-            {
-                sb.AppendLine($"  {item.HashString}.{item.Name}");
-            }
-            sb.AppendLine();
-            sb.AppendLine("HashList:");
-            foreach (var item in manifest.HashList)
-            {
-                sb.AppendLine($"  {item}");
-            }
-            sb.AppendLine();
-            sb.AppendLine("Unknown A:");
-            foreach (var item in manifest.UnknownListA)
-            {
-                sb.AppendLine($" {item.Item1} {item.Item2} {item.Item3}");
-            }
-            sb.AppendLine();
-            sb.AppendLine("Unknown B:");
-            foreach (var item in manifest.UnknownListB)
-            {
-                sb.AppendLine($" {item}");
-            }
-            var outputPath = Path.GetFullPath(Path.Combine(exportParameters.OutputDirectory, exportParameters.FileRecord.Name + exportParameters.FileExtension));
-            File.WriteAllText(outputPath, sb.ToString());
-
-            exportParameters.OnProgressReport?.Invoke(exportParameters.FileRecord, 0);
-        }
-
-        internal void ExportRecords(List<ManifestEntry> recordsToExport)
-        {
-            if (recordsToExport.Count == 0)
+            var view = CurrentResourceView.DataContext as ResourceViewModels.ManifestResourceViewModel;
+            if (view == null)
             {
                 return;
             }
 
-            var dialog = new SaveFileDialog();
-            dialog.Filter = ExportFilter;
-
-            if (recordsToExport.Count == 1)
+            using (var bagStream = File.OpenRead(ParentViewModel.BagPath))
             {
-                dialog.FileName = recordsToExport[0].Name;
+                view.InitFromRecord(bagStream, SelectedRecord);
             }
-            else
-            {
-                dialog.FileName = "Multiple Files";
-            }
+        }
 
-            if (dialog.ShowDialog() == true)
-            {
-                var outputDirectory = Path.GetDirectoryName(dialog.FileName);
 
-                var exportManifestViewModel = new ExportManifestViewModel
-                {
-                    RecordsToExport = recordsToExport,
-                    OutputDirectory = outputDirectory,
-                };
+        protected override void CustomFileExport(ExportParameters exportParameters)
+        {
+            //ManifestResource manifest;
+            //using (var bagStream = File.OpenRead(ParentViewModel.BagPath))
+            //{
+            //    manifest = new ManifestResource();
+            //    manifest.InitFromRecord(bagStream, SelectedRecord);
+            //    ManifestList = manifest.Entries;
+            //}
+            //
+            //var sb = new StringBuilder();
+            //sb.AppendLine("Entries:");
+            //foreach (var item in manifest.Entries)
+            //{
+            //    sb.AppendLine($"  {item.HashString}.{item.Name}");
+            //}
+            //sb.AppendLine();
+            //sb.AppendLine("HashList:");
+            //foreach (var item in manifest.HashList)
+            //{
+            //    sb.AppendLine($"  {item}");
+            //}
+            //sb.AppendLine();
+            //sb.AppendLine("Unknown A:");
+            //foreach (var item in manifest.UnknownListA)
+            //{
+            //    sb.AppendLine($" {item.Item1} {item.Item2} {item.Item3}");
+            //}
+            //sb.AppendLine();
+            //sb.AppendLine("Unknown B:");
+            //foreach (var item in manifest.UnknownListB)
+            //{
+            //    sb.AppendLine($" {item}");
+            //}
+            //var outputPath = Path.GetFullPath(Path.Combine(exportParameters.OutputDirectory, exportParameters.FileRecord.Name + exportParameters.FileExtension));
+            //File.WriteAllText(outputPath, sb.ToString());
+            //
+            //exportParameters.OnProgressReport?.Invoke(exportParameters.FileRecord, 0);
+        }
 
-                var exportDialog = new ExportManifestView
-                {
-                    DataContext = exportManifestViewModel
-                };
-                exportDialog.ShowDialog();
-            }
+        internal void ExportRecords(List<ManifestEntry> recordsToExport)
+        {
+            //if (recordsToExport.Count == 0)
+            //{
+            //    return;
+            //}
+            //
+            //var dialog = new SaveFileDialog();
+            //dialog.Filter = ExportFilter;
+            //
+            //if (recordsToExport.Count == 1)
+            //{
+            //    dialog.FileName = recordsToExport[0].Name;
+            //}
+            //else
+            //{
+            //    dialog.FileName = "Multiple Files";
+            //}
+            //
+            //if (dialog.ShowDialog() == true)
+            //{
+            //    var outputDirectory = Path.GetDirectoryName(dialog.FileName);
+            //
+            //    var exportManifestViewModel = new ExportManifestViewModel
+            //    {
+            //        RecordsToExport = recordsToExport,
+            //        OutputDirectory = outputDirectory,
+            //    };
+            //
+            //    var exportDialog = new ExportManifestView
+            //    {
+            //        DataContext = exportManifestViewModel
+            //    };
+            //    exportDialog.ShowDialog();
+            //}
         }
     }
 }
---
+++ SanBag/ViewModels/ResourceViewModels/ManifestResourceViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibSanBag.FileResources;
+using SanBag.Commands;
+
+namespace SanBag.ViewModels.ResourceViewModels
+{
+    public class ManifestResourceViewModel : BaseViewModel
+    {
+        public readonly string FilterNone = "None";
+
+        public CommandManifestExportSelected CommandManifestExportSelected { get; set; }
+
+        private List<ManifestResource.ManifestEntry> _manifestList = new List<ManifestResource.ManifestEntry>();
+
+        public List<ManifestResource.ManifestEntry> ManifestList
+        {
+            get => _manifestList;
+            set
+            {
+                _manifestList = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FilteredManifestList));
+                OnPropertyChanged(nameof(Filters));
+            }
+        }
+
+        public List<ManifestResource.ManifestEntry> FilteredManifestList
+        {
+            get
+            {
+                if (CurrentFilter == "None")
+                {
+                    return _manifestList;
+                }
+                return _manifestList.Where(n => n.Name.Contains(CurrentFilter)).ToList();
+            }
+        }
+
+        private string _currentFilter;
+        public string CurrentFilter
+        {
+            get => _currentFilter;
+            set
+            {
+                _currentFilter = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FilteredManifestList));
+            }
+        }
+
+        public List<string> Filters
+        {
+            get
+            {
+                var foundTypes = new HashSet<string>();
+                foreach (var manifest in ManifestList)
+                {
+                    foundTypes.Add(manifest.Name);
+                }
+                var filters = foundTypes.OrderBy(n => n).ToList();
+                filters.Insert(0, FilterNone);
+
+                return filters;
+            }
+        }
+
+        public ManifestResourceViewModel()
+        {
+            Filters.Add(FilterNone);
+            CurrentFilter = FilterNone;
+            //CommandManifestExportSelected = new CommandManifestExportSelected(this);
+        }
+
+        protected override void LoadFromStream(Stream resourceStream)
+        {
+            var resource = new ManifestResource();
+            resource.InitFromStream(resourceStream);
+            ManifestList = resource.Entries;
+            ManifestList = resource.Entries;
+
+            //FileName = resource.Filename;
+            //SourceCode = resource.Source;
+        }
+
+
+        //public void OnSelectedRecordChanged()
+        //{
+        //    try
+        //    {
+        //        using (var bagStream = File.OpenRead(ParentViewModel.BagPath))
+        //        {
+        //            var manifest = new ManifestResource();
+        //            manifest.InitFromRecord(bagStream, SelectedRecord);
+        //            ManifestList = manifest.Entries;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        ManifestList = new List<ManifestEntry>();
+        //    }
+        //}
+    }
+}
