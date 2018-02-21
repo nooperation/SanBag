5bee6b669f4e1f9000bea35fc62805985dfd4ae5
nooperation <madbrahmin@gmail.com>
2018-02-20 20:32:09 -0500

    Added basic handling of json data from atlas requests

---
+++ SanBag/Models/AtlasResponse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SanBag.Models
+{
+    public class AtlasResponse
+    {
+        public Meta Meta { get; set; }
+        public Datum[] Data { get; set; }
+    }
+
+    public class Meta
+    {
+        public int Count { get; set; }
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public int Total { get; set; }
+        public int TotalPages { get; set; }
+    }
+
+    public class Datum
+    {
+        public override string ToString()
+        {
+            return Attributes.Name;
+        }
+ 
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        public int AccessControls { get; set; }
+        public string CompatVersion { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int Curation { get; set; }
+        public string Description { get; set; }
+        public int HeadCount { get; set; }
+        public string Handle { get; set; }
+        public DateTime ModificationTime { get; set; }
+        public string Name { get; set; }
+        public string PersonaName { get; set; }
+        public string PersonaHandle { get; set; }
+        public string PersonaId { get; set; }
+        public string SceneAssetId { get; set; }
+        public string ThumbnailAssetId { get; set; }
+        public string Uri { get; set; }
+        public int ReviewCount { get; set; }
+        public bool Listed { get; set; }
+        public Images Images { get; set; }
+    }
+
+    public class Images
+    {
+        public Grid Grid { get; set; }
+        public Detail Detail { get; set; }
+        public Large Large { get; set; }
+        public Max Max { get; set; }
+    }
+
+    public class Grid
+    {
+        public string Url { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Detail
+    {
+        public string Url { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Large
+    {
+        public string Url { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Max
+    {
+        public string Url { get; set; }
+        public int Width { get; set; }
+    }
+}
--- SanBag/ViewModels/AtlasViewModel.cs
+++ SanBag/ViewModels/AtlasViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using Newtonsoft.Json;
 using SanBag.Commands;
+using SanBag.Models;
 
 namespace SanBag.ViewModels
 {
@@ -25,8 +28,8 @@ namespace SanBag.ViewModels
             }
         }
 
-        private List<string> _searchResults;
-        public List<string> SearchResults
+        private List<Datum> _searchResults;
+        public List<Datum> SearchResults
         {
             get => _searchResults;
             set
@@ -47,18 +50,35 @@ namespace SanBag.ViewModels
             }
         }
 
+        private int _page;
+        public int Page
+        {
+            get => _page;
+            set
+            {
+                _page = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _totalPages;
+        public int TotalPages
+        {
+            get => _totalPages;
+            set
+            {
+                _totalPages = value;
+                OnPropertyChanged();
+            }
+        }
+
         public AtlasViewModel()
         {
             CommandSearch = new CommandSearch(this);
 
-            SearchQuery = "...";
-            SearchResults = new List<string>()
-            {
-                "Foo",
-                "Bar",
-                "Baz"
-            };
-            ExperienceView = "Butts";
+            SearchQuery = "";
+            SearchResults = new List<Datum>();
+            ExperienceView = "TODO";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -69,7 +89,27 @@ namespace SanBag.ViewModels
 
         public void Search(string query)
         {
-            MessageBox.Show($"Searched for '{query}'");
+            try
+            {
+                var perPage = 10;
+                var request = WebRequest.Create($"https://atlas.sansar.com/api/experiences?perPage={perPage}&q={query}");
+                var response = request.GetResponse();
+
+                var responseJson = "";
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    responseJson = sr.ReadToEnd();
+                }
+
+                var results = JsonConvert.DeserializeObject<AtlasResponse>(responseJson);
+                SearchResults = results.Data.ToList();
+                TotalPages = results.Meta.TotalPages;
+                Page = results.Meta.Page;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Failed to search: {e.Message}");
+            }
         }
     }
 }
