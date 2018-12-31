fc0e24d4036098e92a6db69b100f92efa8547228
nooperation <madbrahmin@gmail.com>
2018-12-30 22:16:25 -0500

    Just using text output for ScriptMetadata dump

--- CommonUI/ViewModels/ResourceViewModels/ScriptMetaDataResourceviewModel.cs
+++ CommonUI/ViewModels/ResourceViewModels/ScriptMetaDataResourceviewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using LibSanBag.FileResources;
 
 namespace CommonUI.ViewModels.ResourceViewModels
@@ -30,8 +31,52 @@ namespace CommonUI.ViewModels.ResourceViewModels
             set
             {
                 _currentScript = value;
+                DumpScriptMetadata();
                 OnPropertyChanged();
             }
+        }
+
+        private string _currentScriptString;
+        public string CurrentScriptString
+        {
+            get => _currentScriptString;
+            set
+            {
+                _currentScriptString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void DumpScriptMetadata()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{CurrentScript.ClassName} ({CurrentScript.DisplayName})");
+            if (CurrentScript.Tooltip.Length > 0)
+            {
+                sb.AppendLine($"{CurrentScript.Tooltip}");
+            }
+            sb.AppendLine();
+
+            if(CurrentScript.Properties.Count > 0)
+            {
+                foreach (var property in CurrentScript.Properties)
+                {
+                    sb.AppendLine($"    {property.Name} ({property.Type})");
+                    if (property.Attributes.Count > 0)
+                    {
+                        foreach (var attribute in property.Attributes)
+                        {
+                            var padding = new string(' ', 8 + attribute.Key.Length + 3);
+                            var attributeValue = attribute.Value.Replace("\n", "\n" + padding);
+
+                            sb.AppendLine($"        {attribute.Key} = {attributeValue}");
+                        }
+                    }
+                    sb.AppendLine();
+                }
+            }
+
+            CurrentScriptString = sb.ToString();
         }
 
         protected override void LoadFromStream(Stream resourceStream, string version)
