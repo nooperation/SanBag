3c36b705390e9712ea5be49184bb32d82d7415e0
nooperation <madbrahmin@gmail.com>
2019-01-09 18:52:35 -0500

    Added message to Sanbag and AtlasView to detect if required dependency is available

--- AtlasView/ViewModels/MainWindowViewModel.cs
+++ AtlasView/ViewModels/MainWindowViewModel.cs
@@ -23,6 +23,16 @@ namespace AtlasView.ViewModels
         {
             Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
+            if(!LibSanBag.ResourceUtils.Unpacker.IsAvailable)
+            {
+                MessageBox.Show(
+                    "This program requires additional dependencies to run. Please obtain one of the following DLLs and place it in the directory containing " + System.AppDomain.CurrentDomain.FriendlyName + ":\n" +
+                    "  oo2core_6_win64.dll\n" +
+                    "  oo2core_7_win64.dll",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error
+                );
+            }
+
             try
             {
                 CurrentView = new Views.AtlasView
